06bb63eb0437e6290bdf2ec4e550d566367d322f
JonahBolin <jonah.bolin17@gmail.com>
2025-10-13 15:00:36 +0200

    Header, Screen och Footer

---
+++ Zen_Egg/index.js
@@ -0,0 +1,11 @@
+function loadPage(page) {
+    const appName = document.createElement("h1");
+    appName.id = "ZEN_EGG"
+    appName.textContent = "ZEN EGG";
+}
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadPage("home");
+})
