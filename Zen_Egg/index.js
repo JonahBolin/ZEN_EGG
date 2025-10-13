4fd98454a702f406b89197db5d295ae3e15f61e9
JonahBolin <jonah.bolin17@gmail.com>
2025-10-13 22:51:00 +0200

    Skapat första sidan + övergång

--- Zen_Egg/index.js
+++ Zen_Egg/index.js
@@ -1,24 +1,69 @@
+const phone = document.querySelector(".phone");
+const divHeader = document.querySelector(".divHeader");
 const mainScreen = document.querySelector(".mainScreen");
+const divFooter = document.querySelector(".divFooter");
+
+const homeButton = document.createElement("button");
+homeButton.textContent = "";
+
+const appLogo = document.createElement("img");
+appLogo.src = "./app_logga/egg_heart_zen2.png";
+appLogo.alt = "Zen Egg Logo";
 
 function loadPage(page) {
 
     if (page === "home") {
+
+        divHeader.innerHTML = "";
+        mainScreen.innerHTML = "";
+        divFooter.innerHTML = "";
+
         const appNameParent = document.createElement("div");
         appNameParent.id = "appNameParent";
         const appName = document.createElement("h1");
-        appName.id = "ZEN_EGG"
+        appName.id = "ZenEgg";
         appName.textContent = "ZEN EGG";
 
         appNameParent.appendChild(appName);
         mainScreen.appendChild(appNameParent);
 
+        const appLogoParent = document.createElement("div");
+        appLogoParent.id = "appLogoParent";
+        mainScreen.appendChild(appLogoParent);
+
+        appLogo.classList.add("appLogoBig");
+
+        appLogoParent.appendChild(appLogo);
+
+        const homeButton = createButton("Get your day started");
+        divFooter.appendChild(homeButton);
     }
 
 }
 
+function loadEggChoice() {
+    console.log("Hej!");
 
+    divHeader.innerHTML = "";
+    mainScreen.innerHTML = "";
+    divFooter.innerHTML = "";
+
+    appLogoSmall.classList.replace("appLogoBig", "appLogoSmall");
+    divHeader.appendChild(appLogoSmall);
+
+
+}
 
 window.addEventListener("DOMContentLoaded", () => {
     loadPage("home");
 })
+
+function createButton(text) {
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.classList.add("button");
+    button.addEventListener("click", () => loadEggChoice());
+    return button;
+}
+
 
