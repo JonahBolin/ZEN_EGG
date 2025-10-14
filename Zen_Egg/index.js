d4dd14259a6250d0e743d242f4d62fb0fa17d69d
JonahBolin <jonah.bolin17@gmail.com>
2025-10-14 11:33:22 +0200

    La till element för att välja äggstorlek mm

--- Zen_Egg/index.js
+++ Zen_Egg/index.js
@@ -3,8 +3,15 @@ const divHeader = document.querySelector(".divHeader");
 const mainScreen = document.querySelector(".mainScreen");
 const divFooter = document.querySelector(".divFooter");
 
+//1:a sidan
 const homeButton = document.createElement("button");
 homeButton.textContent = "";
+
+//2:a sidan
+const boilingTimeInfo = document.createElement("p");
+const boilingMinutesVariable = "";
+const boilingTimeDiv = document.createElement("div");
+
 
 const appLogo = document.createElement("img");
 appLogo.src = "./app_logga/egg_heart_zen2.png";
@@ -49,33 +56,88 @@ function loadEggChoice() {
     mainScreen.innerHTML = "";
     divFooter.innerHTML = "";
 
-    const eggConsistencyParent = document.createElement("div");
-    const eggSizeParent = document.createElement("div");
-    const eggAmountParent = document.createElement("div");
+    //section 1
 
+    const eggConsistencyParent = document.createElement("div");
     const eggConsistencyQuestion = document.createElement("p");
     const hardButton = document.createElement("button");
     const softButton = document.createElement("button");
     const runnyButton = document.createElement("button");
-
+    const eggButtonsDiv = document.createElement("div");
 
     eggConsistencyQuestion.classList.add("consistencyQuestion");
     eggConsistencyParent.classList.add("eggConsistencyParent");
-    eggSizeParent.classList.add("eggSizeParent");
-    eggAmountParent.classList.add("eggAmountParent");
 
-    hardButton.classList.add("hardButton");
-    softButton.classList.add("softButton");
-    runnyButton.classList.add("runnyButton");
+    eggButtonsDiv.classList.add("eggButtonsDiv");
+    hardButton.classList.add("hardButton", "eggButton");
+    softButton.classList.add("softButton", "eggButton");
+    runnyButton.classList.add("runnyButton", "eggButton");
 
     eggConsistencyQuestion.textContent = "Choose an egg consistency";
     hardButton.textContent = "Hard";
     softButton.textContent = "Soft";
     runnyButton.textContent = "Runny";
 
-    eggConsistencyParent.append(eggConsistencyQuestion, hardButton, softButton, runnyButton);
+    eggConsistencyParent.appendChild(eggConsistencyQuestion);
+    eggButtonsDiv.append(hardButton, softButton, runnyButton);
+    eggConsistencyParent.appendChild(eggButtonsDiv);
     mainScreen.appendChild(eggConsistencyParent);
 
+    //section 2
+
+    const eggSizeParent = document.createElement("div");
+    const eggSizeChoiceDiv = document.createElement("div");
+    const eggSizeQuestion = document.createElement("p");
+    const eggSizeSButton = document.createElement("button");
+    const eggSizeMButton = document.createElement("button");
+    const eggSizeLButton = document.createElement("button");
+    const eggSizeXLButton = document.createElement("button");
+
+    eggSizeParent.classList.add("eggSizeParent");
+    eggSizeChoiceDiv.classList.add("eggSizeChoiceDiv", "sizeButton");
+    eggSizeQuestion.classList.add("sizeQuestion");
+    eggSizeSButton.classList.add("eggSizeSButton", "sizeButton");
+    eggSizeMButton.classList.add("eggSizeMButton", "sizeButton");
+    eggSizeLButton.classList.add("eggSizeLButton", "sizeButton");
+    eggSizeXLButton.classList.add("eggSizeXLButton", "sizeButton");
+
+    eggSizeChoiceDiv.append(eggSizeSButton, eggSizeMButton, eggSizeLButton, eggSizeXLButton);
+    eggSizeParent.append(eggSizeQuestion, eggSizeChoiceDiv);
+    mainScreen.appendChild(eggSizeParent);
+
+    eggSizeQuestion.textContent = "Specify your egg size";
+    eggSizeSButton.textContent = "S";
+    eggSizeMButton.textContent = "M";
+    eggSizeLButton.textContent = "L";
+    eggSizeXLButton.textContent = "XL";
+
+    //section 3
+
+    const eggAmountParent = document.createElement("div");
+    const amountQuestion = document.createElement("p");
+    const amountInput = document.createElement("input");
+    const amountInputDiv = document.createElement("div");
+    amountInput.type = "number";
+
+    amountQuestion.textContent = "Specify how many eggs";
+
+    eggAmountParent.classList.add("eggAmountParent");
+    amountInput.classList.add("amountInput");
+    amountQuestion.classList.add("amountQuestion");
+    amountInputDiv.classList.add("amountInputDiv");
+
+    eggAmountParent.append(amountQuestion, amountInputDiv);
+    amountInputDiv.appendChild(amountInput);
+    mainScreen.appendChild(eggAmountParent);
+
+    amountInput.addEventListener("input", () => {
+        console.log("egg");
+        const nextButton = createButton("Next");
+        divFooter.appendChild(nextButton);
+
+        // boilingTimeInfo
+
+    })
 }
 
 window.addEventListener("DOMContentLoaded", () => {
